14a150bf25ebd0f5c5a1ac5e1d1b7a25503e8fb4
hhadaroadams <hadaroadams1234@gmail.com>
2025-10-19 22:11:59 +0000

    add api calls to protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,19 +3,22 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 const isProtectedPath = (path: string) => {
-  const protectedPaths = ["/dashboard", "/tasks"];
+  const protectedPaths = ["/dashboard", "/tasks", "/api/task"];
   return protectedPaths.some((protectedPath) => path.startsWith(protectedPath));
 };
 
 export function middleware(request: NextRequest) {
   const currentPath = request.nextUrl.pathname;
-  console.log("Middleware - Current Path:", currentPath);
+  // console.log("Middleware - Current Path:", currentPath);
   if (!isProtectedPath(currentPath)) {
     return NextResponse.next();
   }
 
   const token = request.cookies.get("token")?.value;
   if (!token) {
+    if (currentPath.startsWith("/api")) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
     return NextResponse.redirect(new URL("/login", request.url));
   }
   try {
@@ -25,11 +28,14 @@ export function middleware(request: NextRequest) {
     }
     return NextResponse.next();
   } catch (error) {
+    if (currentPath.startsWith("/api")) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
     return NextResponse.redirect(new URL("/login", request.url));
   }
 }
 
 export const config = {
-  // matcher: ["/dashboard/:path*", "/tasks/:path*"],
+  matcher: ["/dashboard/:path*", "/tasks/:path*", "/api/task/:path*"],
   runtime: "nodejs",
 };
